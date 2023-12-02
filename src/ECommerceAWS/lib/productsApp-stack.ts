884449566f8b15b44a3e569622bb4c870061e85b
danilo.silva <danilo.silva@msn.com>
2023-12-02 14:58:44 -0300

    Criando a tabela do DynamoDB com o AWS CDK

--- src/ECommerceAWS/lib/productsApp-stack.ts
+++ src/ECommerceAWS/lib/productsApp-stack.ts
@@ -3,11 +3,15 @@
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
+import { PartitionKey } from "aws-cdk-lib/aws-appsync";
 
 export class ProductsAppStack extends cdk.Stack {
     // Atributo que armazena a função lambda
     readonly productsFetchHandler: lambdaNodeJs.NodejsFunction;
+    // Atributo que armazena a tabela DynamoDB
+    readonly productsTable: dynamodb.Table;
 
     // scope: Onde a stack será inserida
     // id: Nome da stack
@@ -15,6 +19,25 @@ export class ProductsAppStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         // Construtor da classe pai
         super(scope, id, props);
+
+        // Definição da tabela DynamoDB
+        // tableName: Nome da tabela
+        // removalPolicy: Política de remoção - DESTROY: Remove a tabela quando a stack for destruída
+        // partitionKey: Chave primária
+        // billingMode: Modo de cobrança - PROVISIONED: Cobrança por capacidade provisionada
+        // readCapacity: Capacidade de leitura
+        // writeCapacity: Capacidade de escrita
+        this.productsTable = new dynamodb.Table(this, "ProductsTable", {
+            tableName: "Products",
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            partitionKey: {
+                name: "id",
+                type: dynamodb.AttributeType.STRING
+            },
+            billingMode: dynamodb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity: 1
+        });
 
         // Função lambda que busca os produtos
         // runtime: Versão do Node.js
