1580ba2ec3aff279d4fc8df07873ffc9488d9c12
danilo.silva <danilo.silva@msn.com>
2023-12-02 13:32:59 -0300

    Gerando logs no API Gateway

--- src/ECommerceAWS/lib/ecommerceApi-stack.ts
+++ src/ECommerceAWS/lib/ecommerceApi-stack.ts
@@ -14,10 +14,44 @@ export class ECommerceApiStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
         super(scope, id, props);
 
+        // Definição do recurso de log
+        const logGroup = new cwLogs.LogGroup(this, "ECommerceApiLogs");
+
         // Definição do recurso de API Gateway
+        // restApiName: Nome da API
+        // description: Descrição da API
+        // cloudWatchRole: Permite que a API Gateway crie logs no CloudWatch
+        // deployOptions: Opções de deploy
+        // accessLogDestination: Destino dos logs
+        // accessLogFormat: Formato dos logs
+        // jsonWithStandardFields: Formato padrão dos logs
+        // httpMethod: Método HTTP da requisição
+        // ip: Endereço IP do cliente
+        // protocol: Protocolo HTTP
+        // requestTime: Tempo de requisição
+        // resourcePath: Caminho do recurso
+        // responseLength: Tamanho da resposta
+        // status: Status da resposta
+        // caller: Identificador do usuário
+        // user: Nome do usuário
         const api = new apiGateway.RestApi(this, "ECommerceApi", {
             restApiName: "ECommerceApi",
-            description: "API de E-Commerce"
+            description: "API de E-Commerce",
+            cloudWatchRole: true,
+            deployOptions: {
+                accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    caller: true,
+                    user: true
+                })
+            }
         });
 
         // Definição do recurso de produtos
